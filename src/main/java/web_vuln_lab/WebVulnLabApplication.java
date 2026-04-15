fbfe3579ed6c87b88ed3ebced1375575dcedd026
HuLu338 <g504817771@gmail.com>
2026-04-15 21:25:59 +0800

    Initial commit

---
+++ src/main/java/web_vuln_lab/WebVulnLabApplication.java
@@ -0,0 +1,13 @@
+package web_vuln_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebVulnLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebVulnLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/web_vuln_lab/WebVulnLabApplicationTests.java
@@ -0,0 +1,13 @@
+package web_vuln_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebVulnLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
